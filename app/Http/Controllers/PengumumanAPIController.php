5adebd6c5a1b5e650b43394fb04755ccd33251af
rahmawati141 <manrahma10@gmail.com>
2020-01-23 22:12:03 +0800

    CRUD

---
+++ app/Http/Controllers/PengumumanAPIController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pengumuman;
+use Illuminate\Http\Request;
+
+class PengumumanAPIController extends Controller
+{
+    public function index()
+    {
+        $pengumumans=Pengumuman::orderBy('id','desc')->get();
+
+        return $pengumumans;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input=$request->all();
+
+        $pengumuman=Pengumuman::create($input);
+
+        return $pengumuman;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $pengumuman=Pengumuman::find($id);
+
+        return $pengumuman;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $input=$request->all();
+        
+        $pengumuman=Pengumuman::find($id);
+
+        if(empty($pengumuman)){
+            return response()->json(['messege'=>'data tidak ditemukan'], 404);
+        }
+
+        $pengumuman->update($input);
+
+        return response()->json($pengumuman);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $pengumuman=Pengumuman::find($id);
+
+        if(empty($pengumuman)){
+            return response()->json(['message'=>'data tidak ditemukan'], 404);
+        }
+
+        $pengumuman->delete();
+        
+        return response()->json(['message'=>'data telah dihapus']); 
+    
+    }
+}
